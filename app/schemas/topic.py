1d343ce07e8b5ac92de39a76cd4dfe719980edc4
Akim <akimsmirnov2@gmail.com>
2026-01-06 17:48:52 +0300

    добавлены комментарии

--- app/schemas/topic.py
+++ app/schemas/topic.py
@@ -6,25 +6,31 @@ from typing import Literal
 
 
 class TopicCreate(BaseModel):
+    """Схема для создания новой темы"""
     title:str = Field(min_length=1,max_length=200)
     description:str = ""
 
 class TopicRead(BaseModel):
+    """схема для чтения информации о теме"""
     title:str
     description:str
 
 
 class ResourceCreate(BaseModel):
+    """схема для добавления ресурса к теме"""
     res_type: Literal["link","text"]
     content: str
 
 class LinkResourceCreate(BaseModel):
+    """специализированная схема для ресурса ссылки"""
     url: HttpUrl
 
 class NoteCreate(BaseModel):
+    """схема для добавления заметки к теме"""
     text: str = Field(min_length=1)
 
 class ProgressCreate(BaseModel):
+    """схема для добавления записи о прогрессе по теме"""
     percent: int = Field(ge=0, le=100)
 
 
